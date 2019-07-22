0f7d8f99621a75c602e9e898564b9068ecdaea6c
Nicolas Leclerc <nleclerc@sodifrance.fr>
2019-07-22 16:15:22 +0200

    added login test

--- src/test/java/mac/flanigan/base/LoginTest.java
+++ src/test/java/mac/flanigan/base/LoginTest.java
@@ -1,14 +1,16 @@
 package mac.flanigan.base;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
-import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase{
 	
 	@Test
 	public void loginAsBankManager() throws InterruptedException {
+		
 		driver.findElement(By.cssSelector(OR.getProperty("bmlBtn"))).click();
-		Thread.sleep(3000);
+		Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("addCustBtn"))));
+		
 	}
 }
